0341c5e731fb009fd04087edd52d292d9bbdf77b
knkoji <kingsley.nk.oji@gmail.com>
2018-03-06 20:38:00 -0800

    further corrects head rewrite mistake

--- simple_server/server.js
+++ simple_server/server.js
@@ -9,3 +9,5 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   console.log(__dirname);
 });
+
+module.exports = app;
